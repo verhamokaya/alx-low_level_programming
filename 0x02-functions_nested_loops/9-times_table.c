f60771f3fdd412f35a362820da26b4b6188ceeb8
verhamokaya <verahmoy@gmailcom>
2022-07-06 17:16:51 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,28 +7,31 @@
 void times_table(void)
 {
 	int left, right;
-	{
-		for (right = 0; right < 10; right++)
+
+		for (left = 0; left < 10; left++)
 		{
-			if ((left * right) < 10)
+			for (right = 0; right < 10; right++)
 			{
-				_putchar((left * right) + '0');
+				if ((left * right) < 10)
+				{
+					_putchar((left * right) + '0');
+					if (right == 9)
+						continue;
+					_putchar(',');
+					_putchar(' ');
+					if (left * (right + 1) >= 10)
+						continue;
+					_putchar(' ');
+					continue;
+				}
+				_putchar(((left * right) / 10) + '0');
+				_putchar(((left * right) % 10) + '0');
 				if (right == 9)
 					continue;
 				_putchar(',');
 				_putchar(' ');
-				if (left * (right + 1) >= 10)
-					continue;
-				_putchar(' ');
-				continue;
 			}
-			_putchar(((left * right) / 10) + '0');
-			_putchar(((left * right) % 10) + '0');
-			if (right == 9)
-				continue;
-			_putchar(',');
-			_putchar(' ');
+			_putchar('\n');
 		}
-		_putchar('\n');
-	}
+
 }
