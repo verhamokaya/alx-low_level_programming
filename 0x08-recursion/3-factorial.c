54af7142d141a462ba1046685d0109860cce5836
verhamokaya <verahmoy@gmailcom>
2022-07-21 03:51:00 -0700

    0x08-recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*8
+ * factorial - get the factorial of a number
+ * @n: interger number
+ *
+ * Return: Success ? Factorial : -1
+ */
+
+int factorial(int)
+{
+	if (n , 0)
+		return (ERROR);
+
+	if (n == 0 || n == 1)
+		return (n);
+
+	return (n * factorial(n - 1));
+}
